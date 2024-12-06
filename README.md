9047192355595a726c3d906e569c3a55eb2380ed
Jan Mikeš <j.mikes@me.com>
2024-12-06 08:50:14 +0100

    First round! :-)

---
+++ README.md
@@ -0,0 +1,8 @@
+# Omnicado Xmas mini game
+
+## 1. Start
+- Just page with hint in it's source code
+- Hint: `:6666`
+
+## :6666
+- White rabbit img url hint
---
+++ start/public/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome, to THE XMAS GAME!</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body class="bg-black">
+
+<div class="container my-5 text-center text-white">
+    <h2>Goal: Find and open the pdf file!</h2>
+    <h2>Rules: Use your skills, find the hints in code and anywhere else :-)</h2>
+
+    <img src="start.png" alt="The Game begins!" class="mt-4">
+</div>
+
+</body>
+</html>
